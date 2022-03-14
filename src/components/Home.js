bbde1add301dfe163567485bccd250494b6b2e7e
Annika Hägglund <annika.a.a.hagglund@outlook.com>
2022-03-14 15:08:49 +0100

    Added footer to courses and home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function Home() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
