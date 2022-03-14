e55df564c4a1dd7195707ae3331f43440950f10a
Rebecca Rydgren <becka_91_@hotmail.com>
2022-03-14 14:25:28 +0100

    updated header staff

--- src/components/Staff.js
+++ src/components/Staff.js
@@ -1,37 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 function Staff() {
   return (
     <div>
-      <header>
-        <ul>
-          <li>
-            <Link className="link" to="/">
-              Hem
-            </Link>
-          </li>
-          <li>
-            <Link className="currentPage" to="/Staff">
-              Personal
-            </Link>
-          </li>
-          <li>
-            <Link className="link" to="/Course">
-              Kurser
-            </Link>
-          </li>
-          <li>
-            <Link className="link" to="/Education">
-              Utbildning
-            </Link>
-          </li>
-          <li>
-            <Link className="link" to="/Apply">
-              Ansökan
-            </Link>
-          </li>
-        </ul>
-      </header>
+      <Header />
     </div>
   );
 }
