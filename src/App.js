28d763595f1d1d4d800bb9c0a13022bf794e0cae
Rebecca Rydgren <becka_91_@hotmail.com>
2022-03-14 15:10:29 +0100

    added home to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
 import "./App.css";
+import "./components/Home";
+import Home from "./components/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
