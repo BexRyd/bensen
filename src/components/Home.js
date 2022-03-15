2ee63815dc0279d733e5264b95951fba285b211e
Annika Hägglund <annika.a.a.hagglund@outlook.com>
2022-03-15 11:13:29 +0100

    frontpageimage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Computer from "../img/co.jpg";
 
 function Home() {
   return (
     <div>
       <Header />
+      <img src={Computer} alt="Computer" className="frontPageImg" />
       <Footer />
     </div>
   );
