84db2c1857270b6df7cfca6ff9da3c86bfa1b525
dkingbrandt <dkingbrandt@hotmail.com>
2022-03-18 17:09:28 +0100

    server

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./css/App.css";
+
+import "./components/Home";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
