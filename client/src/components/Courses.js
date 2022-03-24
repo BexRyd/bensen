b996d7abb642ac1e599daabbea4c2a521c1e8ee5
Annika Hägglund <annika.a.a.hagglund@outlook.com>
2022-03-24 11:17:21 +0100

    brought in Staff from Rebeccas page

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -6,7 +6,7 @@ import { get, post, put, remove } from "../utilities/apiCourses"; // get the sam
 import { useState, useEffect } from "react";
 
 function Courses() {
-  // const [teacher, setTeacher] = useState([]);
+  const [teacher, setTeacher] = useState([]);
   const [counter, setCounter] = useState(Date.now());
   const [courseName, setCourseName] = useState([]);
   const [courseDescription, setCourseDescription] = useState([]);
@@ -14,14 +14,15 @@ function Courses() {
   // useEffect(() => {
   //   get("/Courses").then((response) => setCourseName(response.data));
   // }, []);
-  // useEffect(() => {
-  //   get("/Staff").then((response) => setTeacher(response.data));
-  // }, []);
+  useEffect(() => {
+    get("/Staff").then((response) => setTeacher(response.data));
+  }, []);
 
   // const choosenTeacher = (e) => setTeacher(e.target.value);
   const addedCourse = (e) => setCourseName(e.target.value);
   const addedDescription = (e) => setCourseDescription(e.target.value);
   const addedLength = (e) => setCourseLength(e.target.value);
+  // const staffDropdown =
 
   // post("/Courses", {
   //   id: Date.now(),
@@ -47,17 +48,17 @@ function Courses() {
             placeholder="Kursbeskrivning"
             onChange={addedDescription}
           ></input>
-          {/* <div>
+          <div>
             {teacher.map((teachers) => {
               return (
                 <div key={teachers.id}>
-                  <option className="staffName">
+                  <option className="courseStaffName">
                     {`${teachers.firstName} ${teachers.lastName}`}
                   </option>
                 </div>
               );
             })}
-          </div> */}
+          </div>
 
           <input
             className="inputLength"
